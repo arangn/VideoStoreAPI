029a393bdd9715627838c1de0a0962b4483662b0
Naheed Arang <arang.na@gmail.com>
2018-11-05 15:41:35 -0800

    merge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
   has_many :rentals
   has_many :movies, through: :rentals
-  
+
   validates :name, :registered_at, :address, :city, :state, :postal_code, :phone, presence: true
 
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   has_many :rentals
-  
+
   validates :title, :overview, :release_date, :inventory, presence: true
   validates :inventory, numericality: {only_integer: true}
 end
