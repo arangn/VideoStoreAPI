80a6b58b7cc2958ac4c9d686c4274e775090544e
Naheed Arang <arang.na@gmail.com>
2018-11-06 12:13:25 -0800

    fix relations in models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :rentals
+  has_many :customers, through: :rentals
 
   validates :title, :overview, :release_date, :inventory, presence: true
   validates :inventory, numericality: {only_integer: true}
