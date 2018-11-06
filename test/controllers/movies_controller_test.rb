8e03e6c76431c861e067e51837818b572b244946
Naheed Arang <arang.na@gmail.com>
2018-11-06 14:11:56 -0800

    movie controller tests, all passing

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,76 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  MOVIE_FIELDS = %w(id title overview release_date inventory).sort
+
+  def check_response(expected_type:, expected_status: :success)
+    expect(response.header['Content-Type']).must_include 'json'
+
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+    return body
+  end
+
+  describe "index" do
+
+    it "is a real working route" do
+      get movies_path
+      body = check_response(expected_type: Array)
+      expect(body.length).must_equal Movie.count
+
+      body.each do |movie|
+        expect(movie.keys.sort).must_equal MOVIE_FIELDS
+      end
+    end
+
+    it "returns an empty array when there are no movies" do
+
+      Movie.destroy_all
+
+      get movies_path
+      body = check_response(expected_type: Array)
+      expect(body).must_equal []
+
+    end
+  end
+
+  describe 'show' do
+
+    it "retrieves info on one movie" do
+      #arrange
+      movie = Movie.first
+      #act
+      get movie_path(movie)
+      #assert
+      body = check_response(expected_type: Hash)
+      expect(body.keys.sort).must_equal MOVIE_FIELDS
+    end
+    it "does something when the movie DNE" do
+      #arrange
+      movie = Movie.first
+      movie.destroy
+
+      #act
+      get movie_path(movie)
+
+      #assert
+      body = check_response(expected_type: Hash, expected_status: :not_found)
+      expect(body).must_include "errors"
+    end
+  end
+
+  describe "create method" do
+    it "can create a movie" do
+      movie_params = {
+      movie: {
+        title: "Movie",
+        overview: 2018-01-01,
+        release_date: "words words words",
+        inventory: 3
+      }}
+      post movies_path(params: movie_params)
+      body = check_response(expected_type: Hash)
+    end
+  end
+
 end
