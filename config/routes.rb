1462886c9808061c393dee9c8954361c2744959b
Naheed Arang <arang.na@gmail.com>
2018-11-08 09:22:26 -0800

    add check in method

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :show, :create]
   resources :rentals, only: [:index, :show, :create]
   post '/rentals/check-out', to: 'rentals#create'
+  post '/rentals/check-in', to: 'rentals#check_in'
 end
 # get 'recipes', to: 'recipes#index', as: 'recipes'
